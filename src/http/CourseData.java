e497755e1633a75c1faff4c8299c50f4516fdd6a
Dongzy <dzy201415@gmail.com>
2017-05-27 12:16:18 +0800

    update method search

--- src/http/CourseData.java
+++ src/http/CourseData.java
@@ -6,6 +6,8 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Map.Entry;
+
 import org.apache.http.ParseException;
 import org.apache.http.auth.AuthenticationException;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -15,6 +17,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.internal.Streams;
@@ -23,7 +26,6 @@ import com.google.gson.stream.JsonWriter;
 public class CourseData extends NetworkConnection {
 	JsonObject course;
 	JsonArray selected;
-	public JsonObject searchResult;
 	
 	private final String coursestorge = "course.json";
 	private final String selectedstorge = "selected.json";
@@ -48,7 +50,6 @@ public class CourseData extends NetworkConnection {
 	public CourseData(String user, String pass) throws AuthenticationException {
 		course = new JsonObject();
 		selected = new JsonArray();
-		searchResult = new JsonObject();
 		url = "http://jwxt.sustc.edu.cn/jsxsd";
 		username = user;
 		password = pass;
@@ -201,25 +202,20 @@ public class CourseData extends NetworkConnection {
 		return selected;
 	}
 	
-	public void search(String name) { //查找课程
+	public JsonObject search(String name) { //查找课程
 		JsonObject result = new JsonObject();
-		for (CourseRepo repo : CourseRepo.values()) {
-			if (course.get(repo.name()).isJsonArray()) {
-				JsonArray array = course.get(repo.name()).getAsJsonArray();
-				JsonArray tArray = new JsonArray();
-				for (int j = 0; j < array.size(); j++) {
-					JsonObject jsonObject = array.get(j).getAsJsonObject();
-					if (jsonObject.toString().contains(name)) {
-						tArray.add(jsonObject);
-					}
+		for (Entry<String, JsonElement> entry : course.entrySet()) {
+			JsonArray array = entry.getValue().getAsJsonArray();
+			JsonArray tArray = new JsonArray();
+			for (int j = 0; j < array.size(); j++) {
+				JsonObject jsonObject = array.get(j).getAsJsonObject();
+				if (jsonObject.toString().contains(name)) {
+					tArray.add(jsonObject);
 				}
-				result.add(repo.name(), tArray);
 			}
-			else {
-				result.add(repo.name(), new JsonArray());
-			}
+			result.add(entry.getKey(), tArray);
 		}
-		searchResult = result;
+		return result;
 	}
 	
 	@Override
