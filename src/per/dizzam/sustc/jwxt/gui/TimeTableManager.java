54024f985c2fc2469153bd2aff1a2856eef8510b
Dongzy <dzy201415@gmail.com>
2017-09-25 17:59:50 +0800

    add some new function to timetable

--- src/per/dizzam/sustc/jwxt/gui/TimeTableManager.java
+++ src/per/dizzam/sustc/jwxt/gui/TimeTableManager.java
@@ -1,6 +1,9 @@
 package per.dizzam.sustc.jwxt.gui;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map.Entry;
+import java.util.Stack;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
@@ -8,6 +11,8 @@ import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseTrackAdapter;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
@@ -15,10 +20,16 @@ import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeColumn;
+import org.eclipse.swt.widgets.TreeItem;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+
+import per.dizzam.sustc.jwxt.CourseData;
+import per.dizzam.sustc.jwxt.CourseRepo;
 
 public class TimeTableManager {
 	
@@ -64,13 +75,13 @@ public class TimeTableManager {
 						}
 					}
 				}
-				composite.getChildren();
 				fd_l.left = new FormAttachment(0, left);
 				fd_l.right = new FormAttachment(0, left + 15);
 				label.setLayoutData(fd_l);
 //				FontData fd = new FontData("MyFont", 10, SWT.ITALIC);
 //				label.setFont(new Font(scroll.getDisplay(), fd));
 				label.moveAbove(null);
+				computeSize(composite);
 				label.requestLayout();
 				label.addMouseTrackListener(new MouseTrackAdapter() {
 
@@ -116,15 +127,171 @@ public class TimeTableManager {
 		}
 	}
 	
+	private CourseData courseData;
+	private Tree tree;
 	private ScrolledComposite scroll;
 	private ArrayList<Composite> weekList = new ArrayList<>();
 	private ArrayList<Course> courses = new ArrayList<>();
 	private ColorPicker picker;
 
-	public TimeTableManager(Composite parent) {
-		picker = new ColorPicker(parent.getDisplay());
+	public TimeTableManager(Composite boxParent, Composite treeParent, CourseData courseData) {
+		this.courseData = courseData;
+		picker = new ColorPicker(boxParent.getDisplay());
+		tree = new Tree(treeParent, SWT.BORDER | SWT.CHECK);
+		scroll = new ScrolledComposite(boxParent, SWT.H_SCROLL | SWT.V_SCROLL);
+		init();
+	}
+	
+	public void addCourse(JsonObject course) throws IllegalArgumentException {
+		courses.add(new Course(course));
+	}
+
+	private void computeSize(Composite target) {
+		scroll.setMinWidth(target.computeSize(SWT.DEFAULT, SWT.DEFAULT).x * 7 + 20);
+//		int max = 0;
+//		for (Control control : target.getChildren()) {
+//			int tmp = ((FormData)control.getLayoutData()).right.offset;
+//			max = max < tmp ? tmp : max;
+//		}
+//		if (target.getBounds().width < max) {
+//			scroll.setMinWidth(max * 7 + 20);
+//		}
+	}
+
+	public void removeCourse(JsonObject course) {
+		for (Course course1 : courses) {
+			if (course1.getCourse().equals(course)) {
+				courses.remove(course1);
+				course1.dispose();
+				break;
+			}
+		}
+	}
+	
+	public void updateData(String string) {
+		tree.removeAll();
+		JsonObject result = string == null ? courseData.getCourse() : courseData.search(string);
+		for (Entry<String, JsonElement> entry : result.entrySet()) {
+			for (JsonElement element : entry.getValue().getAsJsonArray()) {
+				TreeItem root = null;
+				for (TreeItem item : tree.getItems()) {
+					if (item.getText().equals(CourseRepo.valueOf(entry.getKey()).getName())) {
+						root = item;
+					}
+				}
+				if (root == null) {
+					TreeItem newItem = new TreeItem(tree, SWT.CHECK);
+					newItem.setText(CourseRepo.valueOf(entry.getKey()).getName());
+					root = newItem;
+				}
+				TreeItem parent = null;
+				for (TreeItem item : root.getItems()) {
+					if (item.getText().equals(element.getAsJsonObject().get("kcmc").getAsString())) {
+						parent = item;
+					}
+				}
+				if (parent == null) {
+					TreeItem newItem = new TreeItem(root, SWT.NONE);
+					newItem.setText(0, element.getAsJsonObject().get("kcmc").getAsString());
+					newItem.setText(1, String.valueOf(element.getAsJsonObject().get("xf").getAsInt()));
+					JsonElement e = element.getAsJsonObject().get("pgtj");
+					newItem.setText(3, e.isJsonNull() ? "无" : e.getAsString());
+					parent = newItem;
+				}
+				TreeItem item = new TreeItem(parent, SWT.NONE);
+				JsonElement e1 = element.getAsJsonObject().get("fzmc");
+				item.setText(0, element.getAsJsonObject().get("kcmc").getAsString()
+						+ (e1.isJsonNull() ? "" : "[" + e1.getAsString() + "]"));
+				item.setText(1, String.valueOf(element.getAsJsonObject().get("xf").getAsInt()));
+				JsonElement e2 = element.getAsJsonObject().get("skls");
+				item.setText(2, e2.isJsonNull() ? "无" : e2.getAsString());
+				JsonElement e3 = element.getAsJsonObject().get("pgtj");
+				item.setText(3, e3.isJsonNull() ? "无" : e3.getAsString());
+				item.setData(element);
+			}
+		}
+		for (TreeItem item : tree.getItems()) {
+			item.setExpanded(true);
+		}
+	}
+	
+	private void init() {
+		FormData fd_tree = new FormData();
+		fd_tree.bottom = new FormAttachment(100, -7);
+		fd_tree.right = new FormAttachment(100, -7);
+		fd_tree.top = new FormAttachment(0, 30);
+		fd_tree.left = new FormAttachment(0, 7);
+		tree.setLayoutData(fd_tree);
+		tree.setLinesVisible(true);
+		tree.setHeaderVisible(true);
+
+		TreeColumn trclmnA = new TreeColumn(tree, SWT.NONE);
+		trclmnA.setWidth(275);
+		trclmnA.setText("课程名称");
+
+		TreeColumn treeColumn = new TreeColumn(tree, SWT.NONE);
+		treeColumn.setWidth(36);
+		treeColumn.setText("学分");
+
+		TreeColumn trlclmn_ls = new TreeColumn(tree, SWT.NONE);
+		trlclmn_ls.setWidth(60);
+		trlclmn_ls.setText("老师");
+
+		TreeColumn trlclmn_pgtj = new TreeColumn(tree, SWT.NONE);
+		trlclmn_pgtj.setWidth(275);
+		trlclmn_pgtj.setText("先修课程");
+		tree.addSelectionListener(new SelectionAdapter() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if (e.item instanceof TreeItem) {
+					TreeItem item = (TreeItem) e.item;
+					boolean checked = item.getChecked();
+					item.setChecked(checked);
+					item.setGrayed(false);
+					Stack<TreeItem> items = new Stack<>();
+					items.push(item);
+					while (!items.isEmpty()) {
+						TreeItem item1 = items.pop();
+						item1.setChecked(checked);
+						item1.setGrayed(false);
+						if (item1.getItems().length == 0 && item1.getData() instanceof JsonObject) {
+							if (checked) {
+								addCourse((JsonObject) item1.getData());
+							} else {
+								removeCourse((JsonObject) item1.getData());
+							}
+						}
+						items.addAll(Arrays.asList(item1.getItems()));
+					}
+					TreeItem parent;
+					TreeItem head = item;
+					while ((parent = head.getParentItem()) != null) {
+						if (checked) {
+							parent.setChecked(true);
+							parent.setGrayed(false);
+							for (TreeItem item1 : parent.getItems()) {
+								if (!item1.getChecked() || item1.getGrayed()) {
+									parent.setGrayed(true);
+									break;
+								}
+							}
+						} else {
+							parent.setGrayed(true);
+							parent.setChecked(false);
+							for (TreeItem item1 : parent.getItems()) {
+								if (item1.getChecked()) {
+									parent.setChecked(true);
+									break;
+								}
+							}
+						}
+						head = parent;
+					}
+				}
+			}
+		});
 		
-		scroll = new ScrolledComposite(parent, SWT.H_SCROLL | SWT.V_SCROLL);
 		scroll.setExpandHorizontal(true);
 		scroll.setExpandVertical(true);
 		scroll.setMinWidth(0);
@@ -168,24 +335,5 @@ public class TimeTableManager {
 		scroll.setContent(ver);
 
 		ver.setWeights(new int[] { 2, 9, 9, 9, 9, 9, 9, 9 });
-	}
-	
-	public void addCourse(JsonObject course) throws IllegalArgumentException {
-		check(course);
-		courses.add(new Course(course));
-	}
-
-	public void removeCourse(JsonObject course) {
-		for (Course course1 : courses) {
-			if (course1.getCourse().equals(course)) {
-				courses.remove(course1);
-				course1.dispose();
-				break;
-			}
-		}
-	}
-	
-	private void check(JsonObject course) throws IllegalArgumentException {
-		
 	}
 }
